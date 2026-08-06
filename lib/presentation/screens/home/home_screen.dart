b42b9e9f8029c2d0f25e71b7df9f421d4211df0b
wanderworldvlogs0-arch <wanderworldvlogs0@gmail.com>
2026-08-06 15:05:08 +0530

    Create home_screen.dart

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:dr_tragic_mfa/presentation/providers/subject_provider.dart';
+import 'package:dr_tragic_mfa/presentation/widgets/subject_card.dart';
+import 'package:dr_tragic_mfa/presentation/screens/search/search_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<SubjectProvider>().loadSubjects();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dr.tragicMFA'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const SearchScreen()),
+            ),
+          ),
+        ],
+      ),
+      body: Consumer<SubjectProvider>(
+        builder: (context, provider, _) {
+          if (provider.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (provider.error != null) {
+            return Center(child: Text('Error: ${provider.error}'));
+          }
+
+          return RefreshIndicator(
+            onRefresh: provider.loadSubjects,
+            child: GridView.builder(
+              padding: const EdgeInsets.all(16),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.85,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+              ),
+              itemCount: provider.subjects.length,
+              itemBuilder: (context, index) {
+                final subject = provider.subjects[index];
+                return SubjectCard(subject: subject);
+              },
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: const BottomNavBar(currentIndex: 0),
+    );
+  }
+}
