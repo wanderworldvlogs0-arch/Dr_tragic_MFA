a6e9d7285d783c8a923adee712c44e74493143b6
wanderworldvlogs0-arch <wanderworldvlogs0@gmail.com>
2026-08-06 17:06:08 +0530

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -19,7 +19,7 @@ class AppTheme {
         color: Colors.white,
       ),
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       elevation: 2,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
