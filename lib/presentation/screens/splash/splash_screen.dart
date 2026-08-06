361a55bb95131e3712dfe7971f52f44ffe8dce5b
wanderworldvlogs0-arch <wanderworldvlogs0@gmail.com>
2026-08-06 22:53:44 +0530

    Add files via upload

--- lib/presentation/screens/splash/splash_screen.dart
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../widgets/bottom_nav.dart';
+import '../home/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(const Duration(seconds: 2), () {
       if (mounted) {
         Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (_) => const BottomNav()),
+          MaterialPageRoute(builder: (_) => const HomeScreen()),
         );
       }
     });
